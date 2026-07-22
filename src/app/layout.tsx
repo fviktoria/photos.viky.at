c27ad311321bc35433c047468ca2e2d1ac20ac58
Viktoria Ferstl <vikal.fe@icloud.com>
2026-07-22 10:51:50 +0200

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,18 @@ export const metadata: Metadata = {
     locale: "en_AT",
     type: "website",
   },
+  icons: {
+    icon: [
+      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+    ],
+    shortcut: "/favicon.ico",
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+  },
+  manifest: "/site.webmanifest",
+  appleWebApp: {
+    title: "photos.viky",
+  },
 };
 
 export default function RootLayout({
